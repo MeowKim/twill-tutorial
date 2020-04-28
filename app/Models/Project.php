35b92cfaaf9ea578762e2f6db799881d37e188eb
MeowKim <ariesmilk51@gmail.com>
2020-04-28 15:28:17 +0900

    Add dev dependecy: barryvdh/laravel-ide-helper

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -6,6 +6,52 @@ use A17\Twill\Models\Behaviors\HasSlug;
 use A17\Twill\Models\Behaviors\HasMedias;
 use A17\Twill\Models\Model;
 
+/**
+ * App\Models\Project
+ *
+ * @property int $id
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property int $published
+ * @property string|null $title
+ * @property string|null $description
+ * @property string|null $headline
+ * @property string|null $url
+ * @property-read mixed $domain
+ * @property-read mixed $slug
+ * @property-read \Illuminate\Database\Eloquent\Collection|\A17\Twill\Models\Media[] $medias
+ * @property-read int|null $medias_count
+ * @property-write mixed $publish_start_date
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Slugs\ProjectSlug[] $slugs
+ * @property-read int|null $slugs_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Cartalyst\Tags\IlluminateTag[] $tags
+ * @property-read int|null $tags_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\A17\Twill\Models\Model draft()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Project forFallbackLocaleSlug($slug)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Project forInactiveSlug($slug)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Project forSlug($slug)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Project newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Project newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\A17\Twill\Models\Model onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|\A17\Twill\Models\Model published()
+ * @method static \Illuminate\Database\Eloquent\Builder|\A17\Twill\Models\Model publishedInListings()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Project query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\A17\Twill\Models\Model visible()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Project whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Project whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Project whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Project whereHeadline($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Project whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Project wherePublished($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\A17\Twill\Models\Model whereTag($tags, $type = 'slug')
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Project whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Project whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Project whereUrl($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\A17\Twill\Models\Model withTag($tags, $type = 'slug')
+ * @method static \Illuminate\Database\Eloquent\Builder|\A17\Twill\Models\Model withoutTag($tags, $type = 'slug')
+ * @mixin \Eloquent
+ */
 class Project extends Model
 {
     use HasSlug, HasMedias;
--- app/Models/Slugs/ProjectSlug.php
+++ app/Models/Slugs/ProjectSlug.php
@@ -4,6 +4,41 @@ namespace App\Models\Slugs;
 
 use A17\Twill\Models\Model;
 
+/**
+ * App\Models\Slugs\ProjectSlug
+ *
+ * @property int $id
+ * @property int $project_id
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string $slug
+ * @property string $locale
+ * @property int $active
+ * @property-write mixed $publish_start_date
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Cartalyst\Tags\IlluminateTag[] $tags
+ * @property-read int|null $tags_count
+ * @method static \Illuminate\Database\Eloquent\Builder|\A17\Twill\Models\Model draft()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Slugs\ProjectSlug newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Slugs\ProjectSlug newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\A17\Twill\Models\Model onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|\A17\Twill\Models\Model published()
+ * @method static \Illuminate\Database\Eloquent\Builder|\A17\Twill\Models\Model publishedInListings()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Slugs\ProjectSlug query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\A17\Twill\Models\Model visible()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Slugs\ProjectSlug whereActive($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Slugs\ProjectSlug whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Slugs\ProjectSlug whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Slugs\ProjectSlug whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Slugs\ProjectSlug whereLocale($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Slugs\ProjectSlug whereProjectId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Slugs\ProjectSlug whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\A17\Twill\Models\Model whereTag($tags, $type = 'slug')
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Slugs\ProjectSlug whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\A17\Twill\Models\Model withTag($tags, $type = 'slug')
+ * @method static \Illuminate\Database\Eloquent\Builder|\A17\Twill\Models\Model withoutTag($tags, $type = 'slug')
+ * @mixin \Eloquent
+ */
 class ProjectSlug extends Model
 {
     protected $table = "project_slugs";

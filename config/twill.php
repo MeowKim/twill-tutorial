19bf05ac8f1d7cbbccca2500e575e4bd36aff2f2
MeowKim <ariesmilk51@gmail.com>
2020-05-11 09:47:23 +0900

    Change storage back to local. glide's crop didn't work..

--- config/twill.php
+++ config/twill.php
@@ -1,32 +1,35 @@
 <?php
 
-use Aws\S3\S3Client;
-use League\Flysystem\AwsS3v3\AwsS3Adapter;
-use League\Flysystem\Filesystem;
+// use Aws\S3\S3Client;
+// use League\Flysystem\AwsS3v3\AwsS3Adapter;
+// use League\Flysystem\Filesystem;
 
-$client = new S3Client([
-	'credentials' => [
-		'key'    => env('S3_KEY'),
-		'secret' =>  env('S3_SECRET'),
-	],
-	'region' => env('S3_REGION'),
-	'version' => 'latest',
-]);
+// $client = new S3Client([
+// 	'credentials' => [
+// 		'key'    => env('S3_KEY'),
+// 		'secret' =>  env('S3_SECRET'),
+// 	],
+// 	'region' => env('S3_REGION'),
+// 	'version' => 'latest',
+// ]);
 
-$adapter = new AwsS3Adapter($client, env('S3_BUCKET'));
+// $adapter = new AwsS3Adapter($client, env('S3_BUCKET'));
 
-$filesystem = new Filesystem($adapter);
+// $filesystem = new Filesystem($adapter);
 
 return [
-	'glide' => [
-		'source' => env('GLIDE_SOURCE',  $filesystem),
-		'cache' => env('GLIDE_CACHE', $filesystem),
-		'cache_path_prefix' => env('GLIDE_CACHE_PATH_PREFIX', 'glide_cache'),
-		'base_url' => env('GLIDE_BASE_URL', env('S3_BUCKET_URL')),
-		'base_path' => env('GLIDE_BASE_PATH', ''),
-		'use_signed_urls' => env('GLIDE_USE_SIGNED_URLS', false),
-		'sign_key' => env('GLIDE_SIGN_KEY'),
-	],
+	// 'glide' => [
+	// 	'source' => env('GLIDE_SOURCE',  $filesystem),
+	// 	// 'cache' => env('GLIDE_CACHE', $filesystem),
+	// 	'cache' => env('GLIDE_CACHE', storage_path('app')),
+	// 	'cache_path_prefix' => env('GLIDE_CACHE_PATH_PREFIX', 'glide_cache'),
+	// 	// 'base_url' => env('GLIDE_BASE_URL', env('S3_BUCKET_URL')),
+	// 	'base_url' => env('GLIDE_BASE_URL', config('app.url')),
+	// 	// 'base_path' => env('GLIDE_BASE_PATH', ''),
+	// 	'base_path' => env('GLIDE_BASE_PATH', 'img'),
+	// 	'use_signed_urls' => env('GLIDE_USE_SIGNED_URLS', false),
+	// 	'sign_key' => env('GLIDE_SIGN_KEY'),
+	// ],
 	'dashboard' => [
 		'modules' => [
 			'App\Models\Project' => [
